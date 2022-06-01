44592a4322f9802cc4e2af49c7cc77ce6b1e9553
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-06-01 12:27:41 -0300

    Adjust MessageHandler controller and OutputMessage model

--- src/main/java/br/com/jamesson/controller/MessageHandler.java
+++ src/main/java/br/com/jamesson/controller/MessageHandler.java
@@ -4,15 +4,17 @@ import br.com.jamesson.model.Message;
 import br.com.jamesson.model.OutputMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+@Controller
 public class MessageHandler {
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public OutputMessage send(Message message){
+    public OutputMessage send(Message message) throws Exception{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         String time = LocalTime.now().format(formatter);
         return new OutputMessage(message.getFrom(), message.getText(), time);
--- src/main/java/br/com/jamesson/model/OutputMessage.java
+++ src/main/java/br/com/jamesson/model/OutputMessage.java
@@ -11,4 +11,28 @@ public class OutputMessage {
         this.text = text;
         this.time = time;
     }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
